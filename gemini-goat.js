349a0c2b0f373359f591be91106abe1855fda319
Dipto_008 <142109281+Blankid018@users.noreply.github.com>
2024-04-13 21:59:44 +0600

    Update gemini-goat.js

--- gemini-goat.js
+++ gemini-goat.js
@@ -19,7 +19,7 @@ module.exports = {
    if (event.type === "message_reply") {
   var t = event.messageReply.attachments[0].url;
     try {
-      const response= await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/gemini?prompt=${encodeURIComponent(prompt)}&url=${encodeURIComponent(t)}`)
+      const response= await axios.get(`https://noobs-api.onrender.com/dipto/gemini?prompt=${encodeURIComponent(prompt)}&url=${encodeURIComponent(t)}`)
       const data2 = response.data.dipto;
   api.sendMessage(data2, event.threadID, event.messageID);
   } catch (error) {
@@ -32,7 +32,7 @@ else if(!prompt) {
    return api.sendMessage('Please provide a prompt or message reply', event.threadID, event.messageID);}
     else {
   try {
-    const respons = await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/gemini?prompt=${encodeURIComponent(prompt)}`)
+    const respons = await axios.get(`https://noobs-api.onrender.com/dipto/gemini?prompt=${encodeURIComponent(prompt)}`)
     const message = respons.data.dipto;
     api.sendMessage(message, event.threadID,event.messageID);
     } catch (error) {

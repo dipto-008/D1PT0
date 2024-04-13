80b6614e4f8e7d3cf0a039e6a85c7e8106546ecd
Dipto_008 <142109281+Blankid018@users.noreply.github.com>
2024-04-13 23:45:05 +0600

    Create claude2-goat.js
    Oo

---
+++ claude2-goat.js
@@ -0,0 +1,61 @@
+global.GoatBot.config = {
+  api:"https://noobs-api.onrender.com/dipto"
+}
+const axios = require("axios");
+
+module.exports.config = {
+  name: "claude2",
+  version: "1.0.0",
+  role: 0, 
+  author: "dipto", 
+  description: "Claude ai with multiple conversation",
+  usePrefix: true,
+  guide: "[message]",
+  category: "Ai",
+  coolDowns: 5,
+};
+module.exports.onReply = async function ({ api, event, Reply}) {
+ //api.unsendMessage(Reply.messageID);
+  const { author } = Reply;
+  if(author != event.senderID)
+  return;
+  const uid = event.senderID
+  if (event.type == "message_reply") {
+  const reply = event.body.toLowerCase();;
+  if (isNaN(reply)) {
+    const response = await axios.get(`${global.GoatBot.config.api}/claude2?text=${encodeURIComponent(reply)}&senderID=${uid}`)
+       const ok = response.data.data;
+    await api.sendMessage(ok ,event.threadID,(error, info) => {
+  global.GoatBot.onReply.set(info.messageID,{
+        commandName: this.config.name,
+        type: 'reply',
+    messageID: info.messageID,
+    author: event.senderID,
+    link: ok
+  })},event.messageID)
+  }
+  }
+}
+module.exports.onStart = async function ({ api, args, event }) {
+ const uid = event.senderID
+  try {
+    const dipto = args.join(" ").toLowerCase();
+    if (!args[0]) {
+      api.sendMessage(
+        "Please provide a question to answer\n\nExample:\nclaude2 hey",
+  event.threadID,  event.messageID ); return;}
+    if (dipto) {
+      const response = await axios.get(`${global.GoatBot.config.api}/claude2?text=${encodeURIComponent(dipto)}&senderID=${uid}`);
+         const mg = response.data.data;
+      await api.sendMessage({body: mg ,},event.threadID,(error, info) => {
+  global.GoatBot.onReply.set(info.messageID,{
+        commandName: this.config.name,
+    type: 'reply',
+    messageID: info.messageID,
+    author: event.senderID,
+    link: mg
+  })},event.messageID);
+    }
+  } catch (error) {console.error(`Failed to get an answer: ${error.message}`);
+api.sendMessage(`${error.message}.\nAn error`,event.threadID,event.messageID);}
+};

ceae61e2866f4baf417066872b7b7d11677a7ee5
Dipto_008 <142109281+Blankid018@users.noreply.github.com>
2024-06-11 23:02:58 +0600

    Create ss-mirai.js

---
+++ ss-mirai.js
@@ -0,0 +1,30 @@
+const link = "https://noobs-api2.onrender.com/dipto"
+const axios = require('axios')
+module.exports.config = {
+  name: "ss",
+  version: "1.0",
+  author: "Dipto",
+  role: 2,
+  description: "Take a screenshot of a website",
+  category: "utility",
+  guide: {en:"screenshot [URL]"},
+  coolDowns: 5
+};
+module.exports.run = async function ({api, event, args }) {
+  const url = args.join(" "); 
+  if (!url) {
+    return api.sendMessage('Please provide a URL.', event.threadID);
+  }
+  try {
+    const res = await axios.get(`${link}/ss?url=${url}`,{ responseType:'stream' })
+    
+    api.sendMessage(
+      { body: "Screenshot Saved <😽", attachment:res.data }, 
+      event.threadID,
+      event.messageID
+    );
+  } catch (error) {
+    console.error('Error taking screenshot:', error);
+    api.sendMessage('Failed to take a screenshot.', event.threadID);
+  }
+};

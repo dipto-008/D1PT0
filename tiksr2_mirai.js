dacf16fb611ba4a46f62b183bdfea3994f0d5702
Tanvir Ahmed <tanvirahmed309939@gmail.com>
2024-07-15 19:45:11 +0600

    Update tiksr2_mirai.js

--- tiksr2_mirai.js
+++ tiksr2_mirai.js
@@ -15,7 +15,9 @@ module.exports.config = {
     hasPermssion: 0,
     description: "Search for TikTok videos",
     commandCategory: "MEDIA",
+    category: "MEDIA",
     usePrefix: true,
+    prefix: true,
     usages: "<search> - <optional: number of results | blank>",
 };
 

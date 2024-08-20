2ff2c859ef233fbee75624ffd35a92e9f2029b3b
Dipto_008 <142109281+Blankid018@users.noreply.github.com>
2024-08-21 02:52:04 +0600

    Create snippet-mirai.js

---
+++ snippet-mirai.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
+
+module.exports.config = {
+    name: 'snippet',
+    aliases: ['snip'],
+    version: '1.0',
+    hasPermssion: 0,
+    countDowns: 5,
+    credits: 'dipto',
+    description: 'This command transforms text image',
+    category: 'command',
+    commandCategory: 'command',
+    usePrefix: true,
+    prefix: true,
+    usages: '[code] []'
+  },
+module.exports.run = async function ({ api,event,args}) {
+  const code = args.join(" ");
+  if(!code) return api.sendMessage('❎ | Please enter code', event.threadID,event.messageID)
+    try {
+      const { data } = await axios.post(`${await baseApiUrl()}/snippet`, {
+          code: code,
+          lang: 'javascript'
+      });
+      await api.sendMessage({ attachment:  (await require('axios').get(data.imageUrl,{ responseType: 'stream' })).data },event.threadID,event.messageID);
+    } catch (error) {
+      console.error('Error:', error);
+      api.sendMessage('An error occurred while processing your request.',event.threadID,event.messageID);
+    }
+  };

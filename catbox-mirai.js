9abb4f7ed8c4e74bcca94f4f640b2cd9ff6075d3
Nazrul002 <mohammadnazrul002@gmail.com>
2024-10-03 20:31:33 +0600

    Update catbox-mirai.js

--- catbox-mirai.js
+++ catbox-mirai.js
@@ -1,6 +1,13 @@
 const axios = require("axios");
 
-module.exports.config = {
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`
+  );
+  return base.data.api;
+};
+
+(module.exports.config = {
   name: "catbox",
   version: "1.6.9",
   credits: "Nazrul",
@@ -11,15 +18,19 @@ module.exports.config = {
   description: "Convert mp4/mp3/image to link",
   cooldowns: 5,
   usages: "reply to a mp4/mp3/image to upload in catbox"
-}
+},
 
 module.exports.run = async ({ api, event }) => {
   try {
    const allUrl = event.messageReply?.attachments[0]?.url;
+
+   if (!allUrl) {
+        return api.sendMessage("❌ Please reply to a attachment for Upload..!", event.threadID, event.messageID);
+      };
  
    const msg = await api.sendMessage("✨ Uploading Your attachment.. Please Wait✨", event.threadID);
 
-   const { data } = await axios.get(`https://www.noobs-api.000.pe/dipto/catbox?url=${encodeURIComponent(allUrl)}`);
+   const { data } = await axios.get(`${await baseApiUrl()}/catbox?url=${encodeURIComponent(allUrl)}`);
 
   await api.unsendMessage(msg.messageID);
 
@@ -28,4 +39,4 @@ module.exports.run = async ({ api, event }) => {
   } catch (e) {
     api.sendMessage("❌ error while uploading your attachment.", event.threadID);
   }
-  };
+  });

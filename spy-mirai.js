337ad87b7d80503bb1a43ea013b75da7867c5886
Dipto_008 <142109281+dipto-008@users.noreply.github.com>
2024-10-07 05:04:21 +0600

    Update spy-mirai.js

--- spy-mirai.js
+++ spy-mirai.js
@@ -1,3 +1,11 @@
+const axios = require("axios");
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
+
 module.exports = {
   config: {
     name: "spy",

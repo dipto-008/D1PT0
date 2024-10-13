5917fc5336e06db9792c1b97c295c674c9b06ff9
MBBXMESBAH <155101462+MBBXMESBAH@users.noreply.github.com>
2024-10-14 02:18:01 +0600

    Update sing2_goat.js

--- sing2_goat.js
+++ sing2_goat.js
@@ -3,78 +3,90 @@ const yts = require("yt-search");
 const TinyURL = require('tinyurl');
 
 const baseApiUrl = async () => {
-  const base = await axios.get(
-    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`
-  );
-  return base.data.api;
+    const base = await axios.get(
+        `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`
+    );
+    return base.data.api;
 };
 
 (async () => {
-  global.apis = {
-    diptoApi: await baseApiUrl()
-  };
+    global.apis = {
+        diptoApi: await baseApiUrl()
+    };
 })();
 
-async function getStreamFromURL(url,pathName) {
-  try {
-    const response = await axios.get(url,{
-      responseType: "stream"
-    });
-    response.data.path = pathName;
-    return response.data;
-  }
-  catch (err) {
-    throw err;
-  }
+async function getStreamFromURL(url, pathName) {
+    try {
+        const response = await axios.get(url, {
+            responseType: "stream"
+        });
+        response.data.path = pathName;
+        return response.data;
+    } catch (err) {
+        throw err;
+    }
 }
 
 global.utils = {
-...global.utils,
-getStreamFromURL: global.utils.getStreamFromURL || getStreamFromURL,
-getStreamFromUrl: global.utils.getStreamFromUrl || getStreamFromURL
+    ...global.utils,
+    getStreamFromURL: global.utils.getStreamFromURL || getStreamFromURL
 };
 
 const config = {
-name: "sing2",
-author: "Mesbah Saxx",
-credits: "Mesbah Saxx",
-role: 0,
-hasPermssion: 0,
-description: "",
-usePrefix: true,
-prfix: true,
-category: "media",
-commandCategory: "media",
-cooldowns: 5,
-countDown: 5,
+    name: "sing2",
+    author: "Mesbah Saxx",
+    credits: "Mesbah Saxx",
+    version: "1.0.1",
+    role: 0,
+    hasPermssion: 0,
+    description: "",
+    usePrefix: true,
+    prfix: true,
+    category: "media",
+    commandCategory: "media",
+    cooldowns: 5,
+    countDown: 5,
 }
 
-async function onStart({ api, args, event }) {
-try { 
-const songName = args.join(' ');
-const w = await api.sendMessage(`Searching song "${songName}"... `, event.threadID);
-const r = await yts(songName);
-const videos = r.videos.slice( 0, 50 );
+async function onStart({
+    api,
+    args,
+    event
+}) {
+    try {
+        const songName = args.join(' ');
+        const w = await api.sendMessage(`Searching song "${songName}"... `, event.threadID);
+        const r = await yts(songName);
+        const videos = r.videos.slice(0, 50);
 
-const videoData = videos[Math.floor(Math.random() * videos.length)];
+        const videoData = videos[Math.floor(Math.random() * videos.length)];
 
-const { data: { title, quality, downloadLink } } = await axios.get(`${global.apis.diptoApi}/ytDl3?link=${videoData.videoId}&format=mp3`);
+        const {
+            data: {
+                title,
+                quality,
+                downloadLink
+            }
+        } = await axios.get(`${global.apis.diptoApi}/ytDl3?link=${videoData.videoId}&format=mp3`);
 
-api.unsendMessage(w.messageID)
+        api.unsendMessage(w.messageID)
 
-await api.sendMessage({
-body: `🔖 - 𝚃𝚒𝚝𝚕𝚎: ${title}
+        const o = ".php";
+        const shortenedLink = (await axios.get(`https://tinyurl.com/api-create${o}?url=${encodeURIComponent(downloadLink)}`)).data;
+
+        await api.sendMessage({
+            body: `🔖 - 𝚃𝚒𝚝𝚕𝚎: ${title}
 ✨ - 𝚀𝚞𝚊𝚕𝚒𝚝𝚢: ${quality}
 
-📥 - 𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝙻𝚒𝚗𝚔: ${await TinyURL.shorten(downloadLink)}`,
-attachment: await global.utils.getStreamFromURL(downloadLink)
-}, event.threadID, event.messageID);
-} catch (e) {
-api.sendMessage(e, event.threadID, event.messageID);
-}
+📥 - 𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝙻𝚒𝚗𝚔: ${shortenedLink}`,
+            attachment: await global.utils.getStreamFromURL(downloadLink)
+        }, event.threadID, event.messageID);
+    } catch (e) {
+        api.sendMessage(e, event.threadID, event.messageID);
+    }
 }
 module.exports = {
-config, 
-onStart,
-run: onStart
+    config,
+    onStart,
+    run: onStart
 };

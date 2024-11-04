096947ecf66671e2677bb8904377f8e94e2fb3d0
MBBXMESBAH <155101462+MBBXMESBAH@users.noreply.github.com>
2024-11-04 15:58:58 +0600

    Update sing2_mirai.js

--- sing2_mirai.js
+++ sing2_mirai.js
@@ -57,6 +57,7 @@ async function onStart({ api, args, event }) {
     try {
         let videoID;
         const url = args[0];
+        let w;
 
         if (url && (url.includes("youtube.com") || url.includes("youtu.be"))) {
             videoID = getVideoID(url);
@@ -65,7 +66,7 @@ async function onStart({ api, args, event }) {
             }
         } else {
             const songName = args.join(' ');
-            const w = await api.sendMessage(`Searching song "${songName}"... `, event.threadID);
+       	    w = await api.sendMessage(`Searching song "${songName}"... `, event.threadID);
             const r = await yts(songName);
             const videos = r.videos.slice(0, 50);
 

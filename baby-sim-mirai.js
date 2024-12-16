7f191ef113b34e3f9c1ff38147dc43b03f90959d
Dipto_008 <142109281+dipto-008@users.noreply.github.com>
2024-12-16 18:14:41 +0600

    Update baby-sim-mirai.js

--- baby-sim-mirai.js
+++ baby-sim-mirai.js
@@ -158,7 +158,17 @@ try{
     const body = event.body ? event.body.toLowerCase() : ""
     if(body.startsWith("baby") || body.startsWith("bby") || body.startsWith("janu")){
         const arr = body.replace(/^\S+\s*/, "")
-      if(!arr) return message.reply("Yes 😀, i am here ")
+      if(!arr) {
+                                     await api.sendMessage("Yes 😀, i am here ", event.threadID, (error, info) => {
+          global.client.handleReply.push({
+            name: this.config.name,
+            type: "reply",
+            messageID: info.messageID,
+            author: event.senderID
+          });
+        }, event.messageID,
+      )
+    }
     const a = (await axios.get(`${await baseApiUrl()}/baby?text=${encodeURIComponent(arr)}&senderID=${event.senderID}&font=1`)).data.reply;     
         await api.sendMessage(a, event.threadID, (error, info) => {
           global.client.handleReply.push({

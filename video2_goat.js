8ab3f697c91fe28f70495426fdfbc35f6d7e83d4
MOSTAKIM EDITZ <162802866+Mostakim0978@users.noreply.github.com>
2025-04-11 21:42:07 +0600

    Update video2_goat.js

--- video2_goat.js
+++ video2_goat.js
@@ -3,7 +3,7 @@ const yts = require("yt-search");
 
 const baseApiUrl = async () => {
     const base = await axios.get(
-        `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`
+        `https://raw.githubusercontent.com/Mostakim0978/D1PT0/refs/heads/main/baseApiUrl.json`
     );
     return base.data.api;
 };
@@ -55,7 +55,7 @@ const config = {
 
 async function onStart({ api, args, event }) {
     try {
-        let videoID;
+        let videoID,w;
         const url = args[0];
 
         if (url && (url.includes("youtube.com") || url.includes("youtu.be"))) {
@@ -65,7 +65,7 @@ async function onStart({ api, args, event }) {
             }
         } else {
             const songName = args.join(' ');
-            const w = await api.sendMessage(`Searching song "${songName}"... `, event.threadID);
+             w = await api.sendMessage(`Searching song "${songName}"... `, event.threadID);
             const r = await yts(songName);
             const videos = r.videos.slice(0, 50);
 

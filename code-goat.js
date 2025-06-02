d0a430f6cdd961e04c765096fd852da509cd5ae1
Dipto_008 <142109281+dipto-008@users.noreply.github.com>
2025-06-02 14:11:20 +0600

    Update code-goat.js

--- code-goat.js
+++ code-goat.js
@@ -1,11 +1,6 @@
-const fs = require("fs");
-const request = require("request");
-const baseApiUrl = async () => {
-  const base = await require("axios").get(
-    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
-  );
-  return base.data.api;
-};
+const fs = require('fs');
+const axios = require('axios');
+const apiUrl = "https://www.noobs-api.rf.gd/dipto";
 
 module.exports.config = {
   name: "code",
@@ -14,58 +9,37 @@ module.exports.config = {
   author: "dipto",
   usePrefix: true,
   description: "Convert code into link",
-  category: "goatbot",
+  category: "convert",
   guide: { en: "[filename]/[reply and file name]" },
-  countDown: 1,
+  coolDowns: 1
 };
 
 module.exports.onStart = async function ({ api, event, args }) {
-  const admin = ["your uid here"];
+  const admin = "Admin UID ";
   const fileName = args[0];
   if (!admin.includes(event.senderID)) {
-    api.sendMessage(
-      "⚠ | You do not have permission to use this command.",
-      event.threadID,
-      event.messageID,
-    );
-    return;
+    return api.sendMessage("⚠ | You do not have permission to use this command.", event.threadID, event.messageID);
   }
+
   const path = `scripts/cmds/${fileName}.js`;
+
   try {
-    let code = "";
+    let code = '';
     if (event.type === "message_reply") {
       code = event.messageReply.body;
     } else {
-      code = await fs.promises.readFile(path, "utf-8");
+      code = await fs.promises.readFile(path, 'utf-8');
     }
-    const en = encodeURIComponent(code);
-    const options = {
-      url: `${await baseApiUrl()}/paste`,
-      method: "POST",
-      json: true,
-      body: {
-        code: en,
-        name: `${fileName}.js`,
-      },
-    };
-    request(options, (error, response, body) => {
-      if (error) {
-        api.sendMessage(
-          "Error occurred while processing the command.",
-          event.threadID,
-          event.messageID,
-        );
-        return;
-      }
-      const diptoUrl = body.url;
-      api.sendMessage(diptoUrl, event.threadID, event.messageID);
+
+    const res = await axios.post(`${apiUrl}/paste`, {
+      code,
+      name: fileName,
+      type: 'js'
     });
+
+    api.sendMessage(res.data.url, event.threadID, event.messageID);
   } catch (error) {
-    console.error("An error occurred:", error);
-    api.sendMessage(
-      "Error occurred while processing the command.",
-      event.threadID,
-      event.messageID,
-    );
+    console.error("An error occurred:", error.message);
+    api.sendMessage("❌ | Error occurred while processing the command.", event.threadID, event.messageID);
   }
 };
